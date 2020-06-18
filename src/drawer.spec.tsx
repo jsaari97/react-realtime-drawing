eed7e519632fdf2a354a4a69f1bc4f4b674ce47a
Jim Saari <jim.saari97@gmail.com>
2020-06-18 14:59:04 +0300

    Add more tests

--- src/drawer.spec.tsx
+++ src/drawer.spec.tsx
@@ -1,7 +1,28 @@
+import * as React from 'react';
 import { useRealtimeDrawer } from './drawer';
+import { renderHook } from '@testing-library/react-hooks';
 
 describe('useRealtimeDrawer', () => {
-  it('should be truthy', () => {
-    expect(useRealtimeDrawer).toBeTruthy();
+  it('should return null if no ref handle', async () => {
+    const { result } = renderHook(() => useRealtimeDrawer());
+
+    const refContainer = result.current[0];
+
+    expect(Object.keys(refContainer)).toEqual(['current']);
+    expect(refContainer.current).toBeNull();
+  });
+
+  it('should return ref element', async () => {
+    const { result } = renderHook(() => {
+      const ref = useRealtimeDrawer();
+      const canvas = document.createElement('canvas');
+      React.useImperativeHandle(ref[0], () => canvas);
+      return ref;
+    });
+
+    const refContainer = result.current[0];
+
+    expect(Object.keys(refContainer)).toEqual(['current']);
+    expect(refContainer.current?.tagName).toEqual('CANVAS');
   });
 });
