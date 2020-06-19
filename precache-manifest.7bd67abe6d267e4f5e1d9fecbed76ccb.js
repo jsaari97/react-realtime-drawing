823fec2e902ea0ba1b7ec7ba8a4415e2bb82218c
Jim Saari <jim.saari97@gmail.com>
2020-06-19 14:07:19 +0300

    Updates

---
+++ precache-manifest.7bd67abe6d267e4f5e1d9fecbed76ccb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9aef0e3a5e1e85e9c5ec64a8ac38ef8e",
+    "url": "/react-realtime-drawing/index.html"
+  },
+  {
+    "revision": "a590b46ced74df5164c9",
+    "url": "/react-realtime-drawing/static/css/main.86436881.chunk.css"
+  },
+  {
+    "revision": "abb91d3cfcdfc9548eb5",
+    "url": "/react-realtime-drawing/static/js/2.9ee06be8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-realtime-drawing/static/js/2.9ee06be8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a590b46ced74df5164c9",
+    "url": "/react-realtime-drawing/static/js/main.2ba4cb32.chunk.js"
+  },
+  {
+    "revision": "2ef5ad12d468f0ae5a76",
+    "url": "/react-realtime-drawing/static/js/runtime-main.ce763df9.js"
+  }
+]);
