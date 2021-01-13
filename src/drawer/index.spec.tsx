d2fca0dbefec79b363840a7078b1333778a9f84f
Jim Saari <jim.saari97@gmail.com>
2021-01-13 22:28:59 +0200

    Add better unit tests

--- src/drawer/index.spec.tsx
+++ src/drawer/index.spec.tsx
@@ -1,10 +1,6 @@
 import React from 'react'; // eslint-disable-line no-use-before-define
 import { useRealtimeDrawer } from '.';
-import { renderHook, HookResult } from '@testing-library/react-hooks';
-import { RealtimeDrawerValue } from '../types';
-
-const getRef = (result: HookResult<RealtimeDrawerValue>): RealtimeDrawerValue =>
-  result.current;
+import { renderHook } from '@testing-library/react-hooks';
 
 const wrapper: React.FC = ({ children }) => <div>{children}</div>;
 
@@ -12,17 +8,19 @@ describe('useRealtimeDrawer', () => {
   it('should return null if no ref handle', async () => {
     const { result } = renderHook(() => useRealtimeDrawer(), { wrapper });
 
-    const [ref] = getRef(result);
+    const [ref] = result.current;
 
-    expect(Object.keys(ref)).toEqual(['current']);
     expect(ref.current).toBeNull();
   });
 
-  it('should return ref element', async () => {
+  it('should warn if no parent', async () => {
+    global.console.warn = jest.fn();
+
     const { result } = renderHook(
       () => {
         const ref = useRealtimeDrawer();
         const canvas = document.createElement('canvas');
+
         React.useImperativeHandle(ref[0], () => canvas);
 
         return ref;
@@ -30,9 +28,29 @@ describe('useRealtimeDrawer', () => {
       { wrapper }
     );
 
-    const [ref] = getRef(result);
+    const [ref] = result.current;
 
-    expect(Object.keys(ref)).toEqual(['current']);
+    expect(console.warn).toBeCalled();
+    expect(ref.current?.tagName).toEqual('CANVAS');
+  });
+
+  it('should initialize if parent', async () => {
+    const { result } = renderHook(
+      () => {
+        const ref = useRealtimeDrawer();
+        const parent = document.createElement('div');
+        const canvas = document.createElement('canvas');
+        parent.appendChild(canvas);
+
+        React.useImperativeHandle(ref[0], () => canvas);
+
+        return ref;
+      },
+      { wrapper }
+    );
+
+    const [ref] = result.current;
+
     expect(ref.current?.tagName).toEqual('CANVAS');
   });
 });
