19d9f3c939e90e6e75f0e6bc64163061840637f8
Jim Saari <jim.saari97@gmail.com>
2026-03-08 22:17:20 +0200

    chore: upgrade eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,61 @@
+import js from '@eslint/js'
+import eslintConfigPrettier from 'eslint-config-prettier'
+import reactPlugin from 'eslint-plugin-react'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+
+export default tseslint.config(
+  {
+    ignores: ['**/build/**', '**/dist/**', '**/node_modules/**', '**/.snapshots/**', '**/*.min.js']
+  },
+  {
+    linterOptions: {
+      reportUnusedDisableDirectives: 'off'
+    }
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ['**/*.{js,mjs,cjs,jsx,ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      sourceType: 'module',
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+          legacyDecorators: true
+        }
+      },
+      globals: {
+        ...globals.node,
+        ...globals.browser
+      }
+    },
+    plugins: {
+      react: reactPlugin
+    },
+    settings: {
+      react: {
+        version: '17'
+      }
+    },
+    rules: {
+      ...reactPlugin.configs.flat.recommended.rules,
+      ...eslintConfigPrettier.rules,
+      'space-before-function-paren': 'off',
+      'react/prop-types': 'off',
+      'react/jsx-handler-names': 'off',
+      'react/jsx-fragments': 'off',
+      'react/no-unused-prop-types': 'off',
+      '@typescript-eslint/no-empty-object-type': 'off'
+    }
+  },
+  {
+    files: ['src/**/*.{js,jsx,ts,tsx}'],
+    languageOptions: {
+      globals: {
+        ...globals.jest
+      }
+    }
+  }
+)
